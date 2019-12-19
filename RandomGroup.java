9588d3650dcf67846bbb3a0ad6802ea6ceb336d7
mrseay24 <sidneyeseay@gmail.com>
2019-12-18 20:34:37 -0500

    Add files via upload

---
+++ RandomGroup.java
@@ -0,0 +1,32 @@
+package randomGroup;
+import java.util.*;
+public class RandomGroup {
+
+
+public static void main(String args[]) {
+//Generates 100 random doubles between 0.0 and 100.0
+
+Random random = new Random();
+List<Double> myList = new ArrayList<Double>();
+
+for(int i = 0; i < 100; i++){
+	
+	myList.add(random.nextDouble()*100);
+	
+}
+//Calculates average
+Math.
+	
+
+for (double value: myList){
+
+System.out.println(value+ " ");
+System.out.println(average);
+System.out.println(max);
+}
+
+
+}
+
+
+}
